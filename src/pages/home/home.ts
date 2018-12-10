5141de5a1e23dac6032446fe1d67513805d3b749
RICHARD DE OLIVEIRA LOPES <richardxlopes@gmail.com>
2018-12-10 13:51:17 -0200

    confirmar saida

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginService } from '../../providers/login.service';
 import { LoginPage } from '../login/login';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,32 @@ import { LoginPage } from '../login/login';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private loginService: LoginService) {
+  constructor(public navCtrl: NavController, private loginService: LoginService, public alertCtrl: AlertController) {
 
   }
 
   doLogout() {
-    this.loginService.doLogout();
-    this.navCtrl.setRoot(LoginPage);
+    const confirm = this.alertCtrl.create({
+      title: 'Sair',
+      message: 'Quer mesmo sair?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            this.loginService.doLogout();
+            this.navCtrl.setRoot(LoginPage);
+          }
+        }
+      ]
+    });
+    confirm.present();
+
   }
 
 }
